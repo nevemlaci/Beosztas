ed697408431e8dd4437c858c6b20a4bdca374c0f
Nevemlaci <nevemlaci+github@proton.me>
2024-10-16 20:43:58 +0200

    added menu options for json loading/saving

--- src/main/java/org/nevemlaci/schedule/menu/MenuFile.java
+++ src/main/java/org/nevemlaci/schedule/menu/MenuFile.java
@@ -13,39 +13,78 @@ public class MenuFile extends JMenu {
         super("File");
         JMenuItem exit = new LeftClickableMenuItem("Exit", () -> System.exit(0));
         JMenuItem loadEmployeesFromCSV = new LeftClickableMenuItem("Load employees from CSV...",
-                () -> {
-                    JFileChooser fileChooser = new JFileChooser(FileSettings.getLastOpenDir());
-                    if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-                        try {
-                            File file = fileChooser.getSelectedFile();
-                            Settings.importEmployeesFromCSV(file.toString());
-                            FileSettings.setLastOpenDir(file.getParentFile());
-                            App.Instance().getMainPanel().getEmployee_panel().getTable().getModel().updateEmployeeNames(Settings.getEmployees());
-                            App.Instance().getMainPanel().getEmployee_panel().getTable().refreshCellEditor(Settings.getShifts());
-                        }catch (IOException e){
-                            e.printStackTrace();
-                            System.exit(2);
-                        }
-                    }
-                });
+                this::importEmployeesFromCSVButtonBehavior);
 
         JMenuItem loadShiftsFromCSV = new LeftClickableMenuItem("Load shifts from CSV...",
-                () -> {
-                    JFileChooser fileChooser = new JFileChooser(FileSettings.getLastOpenDir());
-                    if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-                        try {
-                            File file = fileChooser.getSelectedFile();
-                            Settings.importShiftsFromCSV(file.toString());
-                            FileSettings.setLastOpenDir(file.getParentFile());
-                            App.Instance().getMainPanel().getEmployee_panel().getTable().refreshCellEditor(Settings.getShifts());
-                        }catch (IOException e){
-                            e.printStackTrace();
-                            System.exit(2);
-                        }
-                    }
-                });
-        add(exit);
+                this::importShiftsFromCSVButtonBehavior);
+
+        JMenuItem importFromJSON = new LeftClickableMenuItem("Import from JSON...", this::importFromJSONButtonBehavior);
+        JMenuItem exportToJSON = new LeftClickableMenuItem("Export to JSON...", this::exportToJSONButtonBehavior);
         add(loadEmployeesFromCSV);
         add(loadShiftsFromCSV);
+        add(importFromJSON);
+        add(exportToJSON);
+        add(exit);
+    }
+
+    private void importEmployeesFromCSVButtonBehavior () {
+        JFileChooser fileChooser = new JFileChooser(FileSettings.getLastOpenDir());
+        if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            try {
+                File file = fileChooser.getSelectedFile();
+                Settings.importEmployeesFromCSV(file.toString());
+                FileSettings.setLastOpenDir(file.getParentFile());
+                App.Instance().getMainPanel().getEmployee_panel().getTable().getModel().updateEmployeeNames(Settings.getEmployees());
+                App.Instance().getMainPanel().getEmployee_panel().getTable().refreshCellEditor(Settings.getShifts());
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(2);
+            }
+        }
+    }
+
+    private void importShiftsFromCSVButtonBehavior() {
+        JFileChooser fileChooser = new JFileChooser(FileSettings.getLastOpenDir());
+        if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            try {
+                File file = fileChooser.getSelectedFile();
+                Settings.importShiftsFromCSV(file.toString());
+                FileSettings.setLastOpenDir(file.getParentFile());
+                App.Instance().getMainPanel().getEmployee_panel().getTable().refreshCellEditor(Settings.getShifts());
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(2);
+            }
+        }
+    }
+
+    private void importFromJSONButtonBehavior(){
+        JFileChooser fileChooser = new JFileChooser(FileSettings.getLastOpenDir());
+        if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            try {
+                File file = fileChooser.getSelectedFile();
+                Settings.importSettingsFromJSON(file.toString());
+                FileSettings.setLastOpenDir(file.getParentFile());
+                App.Instance().getMainPanel().getEmployee_panel().getTable().getModel().updateEmployeeNames(Settings.getEmployees());
+                App.Instance().getMainPanel().getEmployee_panel().getTable().refreshCellEditor(Settings.getShifts());
+            }catch (IOException e){
+                e.printStackTrace();
+                System.exit(2);
+            }
+        }
+    }
+
+    private void exportToJSONButtonBehavior(){
+        JFileChooser fileChooser = new JFileChooser(FileSettings.getLastOpenDir());
+        if (fileChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+            try {
+                File file = fileChooser.getSelectedFile();
+                Settings.exportSettingsToJSON(file.toString());
+                FileSettings.setLastOpenDir(file.getParentFile());
+            }catch (IOException e){
+                e.printStackTrace();
+                System.exit(2);
+            }
+        }
     }
 }
